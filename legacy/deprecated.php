9417faf4b17a49597882cb4ed0c6f2c0f6b919c2
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-10-13 11:10:20 +0000

    Move all the action hook wrappers used in the Hybrid parent theme to the deprecated.php file.  Themes should directly call do_atomic() in templates than these functions.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@374 dba0f204-706d-4bc1-bc29-8b92e0485636

--- legacy/deprecated.php
+++ legacy/deprecated.php
@@ -263,6 +263,263 @@ function hybrid_post_stylesheets( $deprecated_1 = '', $deprecated_2 = '') {
 	return post_stylesheets_stylesheet_uri( $deprecated_1, $deprecated_2 );
 }
 
+/**
+ * Before HTML.  Loaded just after <body> but before any content is displayed.
+ * @since 0.3.2
+ * @deprecated 0.9.1
+ */
+function hybrid_before_html() {
+	do_atomic( 'before_html' );
+}
+
+/**
+ * After HTML. Loaded just before </body> and after all content.
+ * @since 0.3.2
+ * @deprecated 0.9.1
+ */
+function hybrid_after_html() {
+	do_atomic( 'after_html' );
+}
+
+/**
+ * Added to the header before wp_head().
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_head() {
+	do_atomic( 'head' );
+}
+
+/**
+ * Before the header.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_before_header() {
+	do_atomic( 'before_header' );
+}
+
+/**
+ * Header.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_header() {
+	do_atomic( 'header' );
+}
+
+/**
+ * After the header.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_after_header() {
+	do_atomic( 'after_header' );
+}
+
+/**
+ * Before primary menu.
+ * @since 0.8
+ * @deprecated 0.9.1
+ */
+function hybrid_before_primary_menu() {
+	do_atomic( 'before_primary_menu' );
+}
+
+/**
+ * After primary menu.
+ * @since 0.8
+ * @deprecated 0.9.1
+ */
+function hybrid_after_primary_menu() {
+	do_atomic( 'after_primary_menu' );
+}
+
+/**
+ * Before the container.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_before_container() {
+	do_atomic( 'before_container' );
+}
+
+/**
+ * Before the content.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_before_content() {
+	do_atomic( 'before_content' );
+}
+
+/**
+ * After the content.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_after_content() {
+	do_atomic( 'after_content' );
+}
+
+/**
+ * Before each entry.
+ * @since 0.5
+ * @deprecated 0.9.1
+ */
+function hybrid_before_entry() {
+	do_atomic( 'before_entry' );
+}
+
+/**
+ * After each entry.
+ * @since 0.5
+ * @deprecated 0.9.1
+ */
+function hybrid_after_entry() {
+	do_atomic( 'after_entry' );
+}
+
+/**
+ * After singular views but before the comments template.
+ * @since 0.7
+ * @deprecated 0.9.1
+ */
+function hybrid_after_singular() {
+	if ( is_singular( 'post' ) && !is_attachment() )
+		do_action( 'hybrid_after_single' ); // Deprecated
+	elseif ( is_page() )
+		do_action( 'hybrid_after_page' ); // Deprecated
+
+	do_atomic( 'after_singular' );
+}
+
+/**
+ * Before the primary widget area content.  Only called if Primary is active.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_before_primary() {
+	do_atomic( 'before_primary' );
+}
+
+/**
+ * After the primary widget area content.  Only called if Primary is active.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_after_primary() {
+	do_atomic( 'after_primary' );
+}
+
+/**
+ * Before the secondary widget area.  Only called if Secondary is active.
+ * @since 0.2
+ * @deprecated 0.9.1
+ */
+function hybrid_before_secondary() {
+	do_atomic( 'before_secondary' );
+}
+
+/**
+ * After the secondary widget area.  Only called if Secondary is active.
+ * @since 0.2
+ * @deprecated 0.9.1
+ */
+function hybrid_after_secondary() {
+	do_atomic( 'after_secondary' );
+}
+
+/**
+ * Before the subsidiary widget area.  Only called if Subsidiary is active.
+ * @since 0.3.1
+ * @deprecated 0.9.1
+ */
+function hybrid_before_subsidiary() {
+	do_atomic( 'before_subsidiary' );
+}
+
+/**
+ * After the subsidiary widget area.  Only called if Subsidiary is active.
+ * @since 0.3.1
+ * @deprecated 0.9.1
+ */
+function hybrid_after_subsidiary() {
+	do_atomic( 'after_subsidiary' );
+}
+
+/**
+ * After the container area.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_after_container() {
+	do_atomic( 'after_container' );
+}
+
+/**
+ * Before the footer.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_before_footer() {
+	do_atomic( 'before_footer' );
+}
+
+/**
+ * The footer.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_footer() {
+	do_atomic( 'footer' );
+}
+
+/**
+ * After the footer.
+ * @since 0.1
+ * @deprecated 0.9.1
+ */
+function hybrid_after_footer() {
+	do_atomic( 'after_footer' );
+}
+
+/**
+ * Fires before each comment's information.
+ * @since 0.5
+ * @deprecated 0.9.1
+ */
+function hybrid_before_comment() {
+	do_atomic( 'before_comment' );
+}
+
+/**
+ * Fires after each comment's information.
+ * @since 0.5
+ * @deprecated 0.9.1
+ */
+function hybrid_after_comment() {
+	do_atomic( 'after_comment' );
+}
+
+/**
+ * Fires before the comment list.
+ * @since 0.6
+ * @deprecated 0.9.1
+ */
+function hybrid_before_comment_list() {
+	do_atomic( 'before_comment_list' );
+}
+
+/**
+ * Fires after the comment list.
+ * @since 0.6
+ * @deprecated 0.9.1
+ */
+function hybrid_after_comment_list() {
+	do_atomic( 'after_comment_list' );
+}
+
 /* === Removed Functions === */
 
 /* Functions removed in the 0.5 branch. */
