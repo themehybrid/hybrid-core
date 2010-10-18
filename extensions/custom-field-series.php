c5240bf71e40c14c25de612239dc5cc0a2605ca7
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-10-18 07:24:15 +0000

    Updates for custom field series and an overhaul of its documentation.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@419 dba0f204-706d-4bc1-bc29-8b92e0485636

--- extensions/custom-field-series.php
+++ extensions/custom-field-series.php
@@ -1,28 +1,32 @@
 <?php
 /**
- * Functions for displaying a series of posts linked together
- * by a custom field called 'Series'.  Each post is listed that
- * belong to the same series of posts.
+ * Custom Field Series - A script for creating a series of posts by custom field.
  *
- * @copyright 2007 - 2010
- * @version 0.2.1
- * @author Justin Tadlock
- * @link http://justintadlock.com/archives/2007/11/01/wordpress-custom-fields-listing-a-series-of-posts
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Custom Field Series was created to allow users to add individual posts to a larger series of posts.  It was 
+ * created before WordPress made it easy for developers to create new taxonomies.  Ideally, one would use a 
+ * taxonomy to handle this functionality.  However, this method is lighter, provides an extremely simple 
+ * method for adding posts to a series, and offers backwards compatibility for people that have used this 
+ * method before.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package CustomFieldSeries
+ * @version 0.2.2
+ * @author Justin Tadlock <justin@justintadlock.com>
+ * @copyright Copyright (c) 2007 - 2010, Justin Tadlock
+ * @link http://justintadlock.com/archives/2007/11/01/wordpress-custom-fields-listing-a-series-of-posts
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 /**
- * Grabs series by custom field.  Checks for other articles in the series.
- * Series identified custom field key 'Series' and unique value.
- *
- * @todo Fix the multiple hyphens in the series class.
- * @todo Allow filtering of title.
+ * Checks for a series of posts by the current post's metadata.  The function grabs the meta value for the 
+ * 'Series' meta key and checks if any posts have been given the same value.  If posts are found with this 
+ * meta key/value pair, the function adds them to an unordered list.
  *
  * @since 0.1
  * @param array $args Array of arguments.
@@ -30,14 +34,22 @@
 function custom_field_series( $args = array() ) {
 	global $post;
 
+	/* Set up a default textdomain. */
 	$textdomain = hybrid_get_textdomain();
 
+	/* Set $series to an empty string. */
 	$series = '';
 
-	$series_meta = get_metadata( 'post', $post->ID, 'Series', true );
+	/* Allow developers to overwrite the meta key used for the series name. */
+	$meta_key = apply_filters( 'custom_field_series_meta_key', 'Series' );
 
-	if ( $series_meta ) {
+	/* Get the series meta value for the post. */
+	$meta_value = get_post_meta( $post->ID, $meta_key, true );
 
+	/* If a meta value was found, create a list of posts in the series. */
+	if ( !empty( $meta_value ) ) {
+
+		/* Set up the default post query arguments. */
 		$defaults = array(
 			'order' => 'DESC',
 			'orderby' => 'ID',
@@ -45,46 +57,57 @@ function custom_field_series( $args = array() ) {
 			'exclude' => '',
 			'post_type' => 'any',
 			'numberposts' => -1,
-			'meta_key' => 'Series',
-			'meta_value' => $series_meta,
+			'meta_key' => $meta_key,
+			'meta_value' => $meta_value,
 			'echo' => true
 		);
 
+		/* Allow developers to override the arguments used. */
 		$args = apply_filters( 'custom_field_series_args', $args );
-
 		$args = wp_parse_args( $args, $defaults );
 
+		/* Get all posts in the current series. */
 		$series_posts = get_posts( $args );
 
-		if ( $series_posts ) {
+		/* If posts were found, display them. */
+		if ( !empty( $series_posts ) ) {
 
+			/* Format the series class with the name of the series. */
 			$class = str_replace( array( '_', ' ', '&nbsp;' ) , '-', $series_meta );
 			$class = preg_replace('/[^A-Za-z0-9-]/', '', $class );
 			$class = strtolower( $class );
 
-			$series = '<div class="series series-' . $class . '">';
+			/* Create the opening wrapper div, title, and list element. */
+			$series = '<div class="series series-' . esc_attr( $class ) . '">';
 			$series .= '<h4 class="series-title">' . __( 'Articles in this series', $textdomain) . '</h4>';
 			$series .= '<ul>';
 
+			/* Loop through the posts. */
 			foreach ( $series_posts as $serial ) {
 
+				/* If the current post in the loop matches the post we're viewing, don't link to it. */
 				if ( $serial->ID == $post->ID )
 					$series .= '<li class="current-post">' . $serial->post_title . '</li>';
 
+				/* Display a link to the post. */
 				else
 					$series .= '<li><a href="' . get_permalink( $serial->ID ) . '" title="' . esc_attr( $serial->post_title ) . '">' . $serial->post_title . '</a></li>';
 			}
 
+			/* Close the unordered list and wrapper div. */
 			$series .= '</ul></div>';
 		}
 	}
 
+	/* Allow developers to overwrite the HTML of the series. */
 	$series = apply_filters( 'custom_field_series', $series );
 
-	if ( !empty( $args['echo'] ) && $series )
+	/* If $echo is set to true, display the series. */
+	if ( !empty( $args['echo'] ) )
 		echo $series;
 
-	elseif ( $series )
+	/* If $echo is not set to true, return the series. */
+	else
 		return $series;
 }
 
