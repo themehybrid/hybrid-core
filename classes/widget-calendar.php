8b4e9aeeef6c786c11c280a7662161020b34c27c
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-11-30 20:37:27 +0000

    Code and inline documentation cleanup for the Calendar widget.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@512 dba0f204-706d-4bc1-bc29-8b92e0485636

--- classes/widget-calendar.php
+++ classes/widget-calendar.php
@@ -1,61 +1,92 @@
 <?php
 /**
- * Calendar Widget Class
- *
  * The calendar widget was created to give users the ability to show a post calendar for their blog 
  * using all the available options given in the get_calendar() function. It replaces the default WordPress
  * calendar widget.
- *
- * @since 0.6
- * @link http://codex.wordpress.org/Function_Reference/get_calendar
- * @link http://themehybrid.com/themes/hybrid/widgets
  *
  * @package Hybrid
  * @subpackage Classes
  */
 
+/**
+ * Calendar Widget Class
+ *
+ * @since 0.6.0
+ * @link http://codex.wordpress.org/Function_Reference/get_calendar
+ * @link http://themehybrid.com/themes/hybrid/widgets
+ */
 class Hybrid_Widget_Calendar extends WP_Widget {
 
+	/**
+	 * Prefix for the widget.
+	 * @since 0.7.0
+	 */
 	var $prefix;
+
+	/**
+	 * Textdomain for the widget.
+	 * @since 0.7.0
+	 */
 	var $textdomain;
 
 	/**
 	 * Set up the widget's unique name, ID, class, description, and other options.
-	 * @since 0.6
+	 * @since 0.6.0
 	 */
 	function Hybrid_Widget_Calendar() {
+
+		/* Set the widget prefix. */
 		$this->prefix = hybrid_get_prefix();
+
+		/* Set the widget textdomain. */
 		$this->textdomain = hybrid_get_textdomain();
 
-		$widget_ops = array( 'classname' => 'calendar', 'description' => __( 'An advanced widget that gives you total control over the output of your calendar.', $this->textdomain ) );
-		$control_ops = array( 'width' => 200, 'height' => 350, 'id_base' => "{$this->prefix}-calendar" );
-		$this->WP_Widget( "{$this->prefix}-calendar", __( 'Calendar', $this->textdomain ), $widget_ops, $control_ops );
+		/* Set up the widget options. */
+		$widget_options = array(
+			'classname' => 'calendar',
+			'description' => esc_html__( 'An advanced widget that gives you total control over the output of your calendar.', $this->textdomain )
+		);
+
+		/* Set up the widget control options. */
+		$control_options = array(
+			'width' => 200,
+			'height' => 350,
+			'id_base' => "{$this->prefix}-calendar"
+		);
+
+		/* Create the widget. */
+		$this->WP_Widget( "{$this->prefix}-calendar", esc_attr__( 'Calendar', $this->textdomain ), $widget_options, $control_options );
 	}
 
 	/**
 	 * Outputs the widget based on the arguments input through the widget controls.
-	 * @since 0.6
+	 * @since 0.6.0
 	 */
 	function widget( $args, $instance ) {
 		extract( $args );
 
-		$initial = isset( $instance['initial'] ) ? $instance['initial'] : false;
+		/* Get the $initial argument. */
+		$initial = isset( $instance['initial'] ) ? true : false;
 
+		/* Output the theme's widget wrapper. */
 		echo $before_widget;
 
+		/* If a title was input by the user, display it. */
 		if ( $instance['title'] )
 			echo $before_title . apply_filters( 'widget_title',  $instance['title'], $instance, $this->id_base ) . $after_title;
 
+		/* Display the calendar. */
 		echo '<div class="calendar-wrap">';
 			get_calendar( $initial );
 		echo '</div><!-- .calendar-wrap -->';
 
+		/* Close the theme's widget wrapper. */
 		echo $after_widget;
 	}
 
 	/**
 	 * Updates the widget control options for the particular instance of the widget.
-	 * @since 0.6
+	 * @since 0.6.0
 	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
@@ -70,15 +101,17 @@ class Hybrid_Widget_Calendar extends WP_Widget {
 
 	/**
 	 * Displays the widget control options in the Widgets admin screen.
-	 * @since 0.6
+	 * @since 0.6.0
 	 */
 	function form( $instance ) {
 
-		//Defaults
+		/* Set up the default form values. */
 		$defaults = array(
-			'title' => __( 'Calendar', $this->textdomain ),
+			'title' => esc_attr__( 'Calendar', $this->textdomain ),
 			'initial' => false
 		);
+
+		/* Merge the user-selected arguments with the defaults. */
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
 		<div class="hybrid-widget-controls columns-1">
