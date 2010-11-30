5b48e462c685b416e3fae19bb208c834fd8d2e45
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-11-30 21:53:54 +0000

    Code and inline documentation cleanup for the Nav Menu widget.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@514 dba0f204-706d-4bc1-bc29-8b92e0485636

--- classes/widget-nav-menu.php
+++ classes/widget-nav-menu.php
@@ -1,73 +1,103 @@
 <?php
 /**
- * Nav Menu Widget Class
- *
  * The nav menu widget was created to give users the ability to show nav menus created from the 
  * Menus screen, by the theme, or by plugins using the wp_nav_menu() function.  It replaces the default
  * WordPress navigation menu class.
- *
- * @since 0.8
- * @link http://themehybrid.com/themes/hybrid/widgets
  *
  * @package Hybrid
  * @subpackage Classes
  */
 
+/**
+ * Nav Menu Widget Class
+ *
+ * @since 0.8.0
+ * @link http://themehybrid.com/themes/hybrid/widgets
+ */
 class Hybrid_Widget_Nav_Menu extends WP_Widget {
 
+	/**
+	 * Prefix for the widget.
+	 * @since 0.8.0
+	 */
 	var $prefix;
+
+	/**
+	 * Textdomain for the widget.
+	 * @since 0.8.0
+	 */
 	var $textdomain;
 
 	/**
 	 * Set up the widget's unique name, ID, class, description, and other options.
-	 * @since 0.6
+	 * @since 0.8.0
 	 */
 	function Hybrid_Widget_Nav_Menu() {
+
+		/* Set the widget prefix. */
 		$this->prefix = hybrid_get_prefix();
+
+		/* Set the widget textdomain. */
 		$this->textdomain = hybrid_get_textdomain();
 
-		$widget_ops = array( 'classname' => 'nav-menu', 'description' => __( 'An advanced widget that gives you total control over the output of your menus.', $this->textdomain ) );
-		$control_ops = array( 'width' => 525, 'height' => 350, 'id_base' => "{$this->prefix}-nav-menu" );
-		$this->WP_Widget( "{$this->prefix}-nav-menu", __( 'Navigation Menu', $this->textdomain ), $widget_ops, $control_ops );
+		/* Set up the widget options. */
+		$widget_options = array(
+			'classname' => 'nav-menu',
+			'description' => esc_html__( 'An advanced widget that gives you total control over the output of your menus.', $this->textdomain )
+		);
+
+		/* Set up the widget control options. */
+		$control_options = array(
+			'width' => 525,
+			'height' => 350,
+			'id_base' => "{$this->prefix}-nav-menu"
+		);
+
+		/* Create the widget. */
+		$this->WP_Widget( "{$this->prefix}-nav-menu", esc_attr__( 'Navigation Menu', $this->textdomain ), $widget_options, $control_options );
 	}
 
 	/**
 	 * Outputs the widget based on the arguments input through the widget controls.
-	 * @since 0.6
+	 * @since 0.8.0
 	 */
 	function widget( $args, $instance ) {
 		extract( $args );
 
-		$args = array();
+		/* Set up the arguments for the wp_nav_menu() function. */
+		$args = array(
+			'menu' => 		$instance['menu'],
+			'container' => 		$instance['container'],
+			'container_id' => 		$instance['container_id'],
+			'container_class' => 	$instance['container_class'],
+			'menu_id' => 		$instance['menu_id'],
+			'menu_class' => 		$instance['menu_class'],
+			'link_before' => 		$instance['link_before'],
+			'link_after' => 		$instance['link_after'],
+			'before' => 		$instance['before'],
+			'after' => 		$instance['after'],
+			'depth' => 		intval( $instance['depth'] ),
+			'fallback_cb' => 		$instance['fallback_cb'],
+			'walker' => 		$instance['walker'],
+			'echo' => 		false
+		);
 
-		$args['menu'] = $instance['menu'];
-		$args['container'] = $instance['container'];
-		$args['container_id'] = $instance['container_id'];
-		$args['container_class'] = $instance['container_class'];
-		$args['menu_id'] = $instance['menu_id'];
-		$args['menu_class'] = $instance['menu_class'];
-		$args['link_before'] = $instance['link_before'];
-		$args['link_after'] = $instance['link_after'];
-		$args['before'] = $instance['before'];
-		$args['after'] = $instance['after'];
-		$args['depth'] = intval( $instance['depth'] );
-		$args['fallback_cb'] = $instance['fallback_cb'];
-		$args['walker'] = $instance['walker'];
-		$args['echo'] = false;
-
+		/* Output the theme's widget wrapper. */
 		echo $before_widget;
 
-		if ( $instance['title'] )
+		/* If a title was input by the user, display it. */
 			echo $before_title . apply_filters( 'widget_title',  $instance['title'], $instance, $this->id_base ) . $after_title;
 
+		/* Output the nav menu. */
 		echo str_replace( array( "\r", "\n", "\t" ), '', wp_nav_menu( $args ) );
 
+		/* Close the theme's widget wrapper. */
 		echo $after_widget;
 	}
 
 	/**
 	 * Updates the widget control options for the particular instance of the widget.
-	 * @since 0.6
+	 * @since 0.8.0
 	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
@@ -88,14 +118,13 @@ class Hybrid_Widget_Nav_Menu extends WP_Widget {
 
 	/**
 	 * Displays the widget control options in the Widgets admin screen.
-	 * @since 0.6
+	 * @since 0.8.0
 	 */
 	function form( $instance ) {
 
-		//Defaults
+		/* Set up the default form values. */
 		$defaults = array(
-			'title' => __( 'Navigation', $this->textdomain ),
-			'format' => 'div',
+			'title' => esc_attr__( 'Navigation', $this->textdomain ),
 			'menu' => '',
 			'container' => 'div',
 			'container_id' => '',
@@ -110,6 +139,8 @@ class Hybrid_Widget_Nav_Menu extends WP_Widget {
 			'fallback_cb' => 'wp_page_menu',
 			'walker' => ''
 		);
+
+		/* Merge the user-selected arguments with the defaults. */
 		$instance = wp_parse_args( (array) $instance, $defaults );
 
 		$container = apply_filters( 'wp_nav_menu_container_allowedtags', array( 'div', 'nav' ) );
@@ -124,7 +155,7 @@ class Hybrid_Widget_Nav_Menu extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'menu' ); ?>"><code>menu</code></label> 
 			<select class="widefat" id="<?php echo $this->get_field_id( 'menu' ); ?>" name="<?php echo $this->get_field_name( 'menu' ); ?>">
 				<?php foreach ( wp_get_nav_menus() as $menu ) { ?>
-					<option value="<?php echo $menu->term_id; ?>" <?php selected( $instance['menu'], $menu->term_id ); ?>><?php echo $menu->name; ?></option>
+					<option value="<?php echo esc_attr( $menu->term_id ); ?>" <?php selected( $instance['menu'], $menu->term_id ); ?>><?php echo esc_html( $menu->name ); ?></option>
 				<?php } ?>
 			</select>
 		</p>
@@ -132,7 +163,7 @@ class Hybrid_Widget_Nav_Menu extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'container' ); ?>"><code>container</code></label> 
 			<select class="smallfat" id="<?php echo $this->get_field_id( 'container' ); ?>" name="<?php echo $this->get_field_name( 'container' ); ?>">
 				<?php foreach ( $container as $option ) { ?>
-					<option value="<?php echo $option; ?>" <?php selected( $instance['container'], $option ); ?>><?php echo $option; ?></option>
+					<option value="<?php echo esc_attr( $option ); ?>" <?php selected( $instance['container'], $option ); ?>><?php echo esc_html( $option ); ?></option>
 				<?php } ?>
 			</select>
 		</p>
