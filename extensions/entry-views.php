6570e2d09cbc878f59450b1ed498bd2454df1cf2
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2011-05-17 23:11:47 +0000

    Version bump.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@721 dba0f204-706d-4bc1-bc29-8b92e0485636

--- extensions/entry-views.php
+++ extensions/entry-views.php
@@ -26,7 +26,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package EntryViews
- * @version 0.1.1
+ * @version 0.1.2
  * @author Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2010, Justin Tadlock
  * @link http://justintadlock.com
