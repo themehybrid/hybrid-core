ac1698645d8746cf3ab0ad47cbe775b87b6699ff
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2011-07-22 02:56:48 +0000

    Rolling in the new and improved sidebar code.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@791 dba0f204-706d-4bc1-bc29-8b92e0485636

--- functions/sidebars.php
+++ functions/sidebars.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Sets up the default framework sidebars if the theme supports them.  By default, the framework 
- * registers six sidebars.  Themes may choose to use or not use these sidebars, create new sidebars, or 
- * unregister individual sidebars.  A theme must register support for 'hybrid-core-sidebars' to use them.
+ * Sets up the default framework sidebars if the theme supports them.  By default, the framework registers 
+ * seven sidebars.  Themes may choose to use one or more of these sidebars.  A theme must register support 
+ * for 'hybrid-core-sidebars' to use them and register each sidebar ID within an array for the second 
+ * parameter of add_theme_support().
  *
  * @package HybridCore
  * @subpackage Functions
@@ -12,8 +13,8 @@
 add_action( 'widgets_init', 'hybrid_register_sidebars' );
 
 /**
- * Registers the default framework dynamic sidebars.  Theme developers may optionally choose to support 
- * these sidebars within their themes or add more custom sidebars to the mix.
+ * Registers the default framework dynamic sidebars based on the sidebars the theme has added support 
+ * for using add_theme_support().
  *
  * @since 0.7.0
  * @uses register_sidebar() Registers a sidebar with WordPress.
@@ -21,120 +22,87 @@ add_action( 'widgets_init', 'hybrid_register_sidebars' );
  */
 function hybrid_register_sidebars() {
 
-	/* Get theme-supported sidebars. */
-	$sidebars = get_theme_support( 'hybrid-core-sidebars' );
+	/* Get the theme-supported sidebars. */
+	$supported_sidebars = get_theme_support( 'hybrid-core-sidebars' );
 
-	/* If there is no array of sidebars IDs, return. */
-	if ( !is_array( $sidebars[0] ) )
+	/* If the theme doesn't add support for any sidebars, return. */
+	if ( !is_array( $supported_sidebars[0] ) )
 		return;
+
+	/* Get the available core framework sidebars. */
+	$core_sidebars = hybrid_get_sidebars();
+
+	/* Loop through the supported sidebars. */
+	foreach ( $supported_sidebars[0] as $sidebar ) {
+
+		/* Get the arguments set for the core sidebars if there are any. */
+		$sidebar_args = ( isset( $core_sidebars[$sidebar] ) ? $core_sidebars[$sidebar] : array() );
+
+		/* Set up some default sidebar arguments. */
+		$defaults = array(
+			'before_widget' => 	'<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
+			'after_widget' => 		'</div></div>',
+			'before_title' => 		'<h3 class="widget-title">',
+			'after_title' => 		'</h3>'
+		);
+
+		/* Parse the sidebar arguments and defaults. */
+		$args = wp_parse_args( $sidebar_args, $defaults );
+
+		/* If no 'id' was given, use the $sidebar variable and sanitize it. */
+		$args['id'] = ( isset( $args['id'] ) ? sanitize_key( $args['id'] ) : sanitize_key( $sidebar ) );
+
+		/* Register the sidebar. */
+		register_sidebar( $args );
+	}
+}
+
+/**
+ * Returns an array of the core framework's available sidebars for use in themes.  We'll just set the 
+ * ID (array keys), name, and description of each sidebar.  The other sidebar arguments will be set when the 
+ * sidebar is registered.
+ *
+ * @since 1.2.0
+ */
+function hybrid_get_sidebars() {
 
 	/* Get the theme textdomain. */
 	$domain = hybrid_get_textdomain();
 
-	/* Set up the primary sidebar arguments. */
-	$primary = array(
-		'id' => 'primary',
-		'name' => __( 'Primary', $domain ),
-		'description' => __( 'The main (primary) widget area, most often used as a sidebar.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
+	/* Set up an array of sidebars. */
+	$sidebars = array(
+		'primary' => array(
+			'name' => 	__( 'Primary', $domain ),
+			'description' => 	__( 'The main (primary) widget area, most often used as a sidebar.', $domain )
+		),
+		'secondary' => array(
+			'name' =>	__( 'Secondary', $domain ),
+			'description' =>	__( 'The second most important widget area, most often used as a secondary sidebar.', $domain ),
+		),
+		'subsidiary' => array(
+			'name' => 	__( 'Subsidiary', $domain ),
+			'description' =>	__( 'A widget area loaded in the footer of the site.', $domain ),
+		),
+		'header' => array(
+			'name' =>	__( 'Header', $domain ),
+			'description' =>	__( 'Displayed within the site\'s header area.', $domain ),
+		),
+		'before-content' => array(
+			'name' =>	__( 'Before Content', $domain ),
+			'description' =>	__( 'Loaded before the page\'s main content area.', $domain ),
+		),
+		'after-content' => array(
+			'name' =>	__( 'After Content', $domain ),
+			'description' =>	__( 'Loaded after the page\'s main content area.', $domain ),
+		),
+		'after-singular' => array(
+			'name' =>	__( 'After Singular', $domain ),
+			'description' =>	__( 'Loaded on singular post (page, attachment, etc.) views before the comments area.', $domain ),
+		)
 	);
 
-	/* Set up the secondary sidebar arguments. */
-	$secondary = array(
-		'id' => 'secondary',
-		'name' => __( 'Secondary', $domain ),
-		'description' => __( 'The second most important widget area, most often used as a secondary sidebar.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
-	);
-
-	/* Set up the subsidiary sidebar arguments. */
-	$subsidiary = array(
-		'id' => 'subsidiary',
-		'name' => __( 'Subsidiary', $domain ),
-		'description' => __( 'A widget area loaded in the footer of the site.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
-	);
-
-	/* Set up the header sidebar arguments. */
-	$header = array(
-		'id' => 'header',
-		'name' => __( 'Header', $domain ),
-		'description' => __( 'Displayed within the site\'s header area.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
-	);
-
-	/* Set up the before content sidebar arguments. */
-	$before_content = array(
-		'id' => 'before-content',
-		'name' => __( 'Before Content', $domain ),
-		'description' => __( 'Loaded before the page\'s main content area.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
-	);
-
-	/* Set up the after content sidebar arguments. */
-	$after_content = array(
-		'id' => 'after-content',
-		'name' => __( 'After Content', $domain ),
-		'description' => __( 'Loaded after the page\'s main content area.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
-	);
-
-	/* Set up the after singular sidebar arguments. */
-	$after_singular = array(
-		'id' => 'after-singular',
-		'name' => __( 'After Singular', $domain ),
-		'description' => __( 'Loaded on singular post (page, attachment, etc.) views before the comments area.', $domain ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>'
-	);
-
-	/* Register the primary sidebar. */
-	if ( in_array( 'primary', $sidebars[0] ) )
-		register_sidebar( $primary );
-
-	/* Register the secondary sidebar. */
-	if ( in_array( 'secondary', $sidebars[0] ) )
-		register_sidebar( $secondary );
-
-	/* Register the subsidiary sidebar. */
-	if ( in_array( 'subsidiary', $sidebars[0] ) )
-		register_sidebar( $subsidiary );
-
-	/* Register the header sidebar. */
-	if ( in_array( 'header', $sidebars[0] ) )
-		register_sidebar( $header );
-
-	/* Register the before content sidebar. */
-	if ( in_array( 'before-content', $sidebars[0] ) )
-		register_sidebar( $before_content );
-
-	/* Register the after content sidebar. */
-	if ( in_array( 'after-content', $sidebars[0] ) )
-		register_sidebar( $after_content );
-
-	/* Register the after singular sidebar. */
-	if ( in_array( 'after-singular', $sidebars[0] ) )
-		register_sidebar( $after_singular );
+	/* Return the sidebars. */
+	return $sidebars;
 }
 
 ?>
