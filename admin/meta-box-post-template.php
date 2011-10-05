89e21140dbaa594d9a48cd9204f92f8422753bab
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2011-10-05 19:45:25 +0000

    wp_nonce_field(). Remove add_post_meta(). Use 'edit_post_meta' cap check.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@896 dba0f204-706d-4bc1-bc29-8b92e0485636

--- admin/meta-box-post-template.php
+++ admin/meta-box-post-template.php
@@ -54,7 +54,7 @@ function hybrid_meta_box_post_display_template( $object, $box ) {
 		$templates = hybrid_get_post_templates( array( 'label' => array( "{$post_type_object->labels->singular_name} Template", "{$post_type_object->name} Template" ) ) );
 	} ?>
 
-	<input type="hidden" name="hybrid-core-post-meta-box-template" value="<?php echo wp_create_nonce( basename( __FILE__ ) ); ?>" />
+	<?php wp_nonce_field( basename( __FILE__ ), 'hybrid-core-post-meta-box-template' ); ?>
 
 	<p>
 		<?php if ( 0 != count( $templates ) ) { ?>
@@ -77,6 +77,7 @@ function hybrid_meta_box_post_display_template( $object, $box ) {
  * @since 1.2.0
  * @param int $post_id The ID of the current post being saved.
  * @param int $post The post object currently being saved.
+ * @return void|int
  */
 function hybrid_meta_box_post_save_template( $post_id, $post ) {
 
@@ -84,17 +85,15 @@ function hybrid_meta_box_post_save_template( $post_id, $post ) {
 	if ( !isset( $_POST['hybrid-core-post-meta-box-template'] ) || !wp_verify_nonce( $_POST['hybrid-core-post-meta-box-template'], basename( __FILE__ ) ) )
 		return $post_id;
 
-	/* Get the post type object. */
-	$post_type = get_post_type_object( $post->post_type );
-
-	/* Check if the current user has permission to edit the post. */
-	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+	/* Check if the current user has permission to edit the post meta. */
+	if ( !current_user_can( 'edit_post_meta', $post_id ) )
 		return $post_id;
 
-	/* Get the posted meta value. */
+	/* Return here if the template is not set. There's a chance it won't be if the post type doesn't have any templates. */
 	if ( !isset( $_POST['hybrid-post-template'] ) )
 		return $post_id;
 
+	/* Get the posted meta value. */
 	$new_meta_value = strip_tags( $_POST['hybrid-post-template'] );
 
 	/* Set the $meta_key variable based off the post type name. */
@@ -103,17 +102,13 @@ function hybrid_meta_box_post_save_template( $post_id, $post ) {
 	/* Get the meta value of the meta key. */
 	$meta_value = get_post_meta( $post_id, $meta_key, true );
 
-	/* If a new meta value was added and there was no previous value, add it. */
-	if ( $new_meta_value && '' == $meta_value )
-		add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+	/* If there is no new meta value but an old value exists, delete it. */
+	if ( '' == $new_meta_value && $meta_value )
+		delete_post_meta( $post_id, $meta_key, $meta_value );
 
 	/* If the new meta value does not match the old value, update it. */
 	elseif ( $new_meta_value && $new_meta_value != $meta_value )
 		update_post_meta( $post_id, $meta_key, $new_meta_value );
-
-	/* If there is no new meta value but an old value exists, delete it. */
-	elseif ( '' == $new_meta_value && $meta_value )
-		delete_post_meta( $post_id, $meta_key, $meta_value );
 }
 
 ?>
