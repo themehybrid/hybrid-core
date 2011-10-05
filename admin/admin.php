97595b6aee3b09254ab25680abcfbaf761a7d814
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2011-10-05 18:20:41 +0000

    Move hybrid_image_size_names_choose() to the utility.php so that it's loaded on the front end and backend. This is needed because the new wp_editor() can be used anywhere.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@891 dba0f204-706d-4bc1-bc29-8b92e0485636

--- admin/admin.php
+++ admin/admin.php
@@ -27,9 +27,6 @@ function hybrid_admin_init() {
 
 	/* Loads admin stylesheets for the framework. */
 	add_action( 'admin_enqueue_scripts', 'hybrid_admin_enqueue_styles' );
-
-	/* Add all image sizes to the image editor to insert into post. */
-	add_filter( 'image_size_names_choose', 'hybrid_image_size_names_choose' );
 }
 
 /**
@@ -70,31 +67,6 @@ function hybrid_admin_enqueue_styles( $hook_suffix ) {
 	/* Load admin styles if on the widgets screen and the current theme supports 'hybrid-core-widgets'. */
 	if ( current_theme_supports( 'hybrid-core-widgets' ) && 'widgets.php' == $hook_suffix )
 		wp_enqueue_style( 'hybrid-core-admin' );
-}
-
-/**
- * Adds theme/plugin custom images sizes added with add_image_size() to the image uploader/editor.  This 
- * allows users to insert these images within their post content editor.
- *
- * @since 1.3.0
- * @param array $sizes Selectable image sizes.
- * @return array $sizes
- */
-function hybrid_image_size_names_choose( $sizes ) {
-
-	/* Get all intermediate image sizes. */
-	$intermediate_sizes = get_intermediate_image_sizes();
-	$add_sizes = array();
-
-	/* Loop through each of the intermediate sizes, adding them to the $add_sizes array. */
-	foreach ( $intermediate_sizes as $size )
-		$add_sizes[$size] = $size;
-
-	/* Merge the original array, keeping it intact, with the new array of image sizes. */
-	$sizes = array_merge( $add_sizes, $sizes );
-
-	/* Return the new sizes plus the old sizes back. */
-	return $sizes;
 }
 
 /**
