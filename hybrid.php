10018e2541d7c5d477706c2d6522d5b24ab6feb0
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2011-12-14 03:33:46 +0000

    Hybrid Core 1.3.0 beta 1.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@963 dba0f204-706d-4bc1-bc29-8b92e0485636

--- hybrid.php
+++ hybrid.php
@@ -24,7 +24,7 @@
  * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * @package HybridCore
- * @version 1.3.0
+ * @version 1.3.0-beta-1
  * @author Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2008 - 2011, Justin Tadlock
  * @link http://themehybrid.com/hybrid-core
