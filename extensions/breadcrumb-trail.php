78e363b9a9b359c47351c937dd5dedeb88172fca
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2012-02-15 18:15:52 +0000

    Add missing textdomain.
    git-svn-id: http://svn.locallylost.com/themes/hybrid-core/trunk@981 dba0f204-706d-4bc1-bc29-8b92e0485636

--- extensions/breadcrumb-trail.php
+++ extensions/breadcrumb-trail.php
@@ -457,7 +457,7 @@ function breadcrumb_trail_get_bbpress_items( $args = array() ) {
 
 		if ( bbp_is_single_user_edit() ) {
 			$trail[] = '<a href="' . bbp_get_user_profile_url() . '">' . bbp_get_displayed_user_field( 'display_name' ) . '</a>';
-			$trail[] = __( 'Edit' );
+			$trail[] = __( 'Edit', 'breadcrumb-trail' );
 		} else {
 			$trail[] = bbp_get_displayed_user_field( 'display_name' );
 		}
