4486b5c478481b38741a07306cdf757eaa35f89c
Justin Tadlock <justin@justintadlock.com>
2012-06-04 15:23:56 -0500

    Testing.

--- hybrid.php
+++ hybrid.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Test
  * Hybrid Core - A WordPress theme development framework.
  *
  * Hybrid Core is a framework for developing WordPress themes.  The framework allows theme developers
