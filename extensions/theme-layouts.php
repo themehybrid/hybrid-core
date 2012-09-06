af39d67b30a1e358cc4212fae08335f104276610
Justin Tadlock <justin@justintadlock.com>
2012-09-06 00:01:16 -0500

    Mark as 0.5.0 - Alpha since we're still in the testing stages.

--- extensions/theme-layouts.php
+++ extensions/theme-layouts.php
@@ -19,7 +19,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package ThemeLayouts
- * @version 0.5.0
+ * @version 0.5.0 - Alpha
  * @author Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2010 - 2012, Justin Tadlock
  * @link http://justintadlock.com
