673bfc00c24b7def97f6f103fc1da4ea895cfe38
Justin Tadlock <justin@justintadlock.com>
2012-10-18 10:42:36 -0500

    Change version number of Loop Pagination to 1.6.

--- extensions/loop-pagination.php
+++ extensions/loop-pagination.php
@@ -14,7 +14,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package   LoopPagination
- * @version   0.1.6 - Alpha
+ * @version   0.1.6
  * @author    Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2010 - 2012, Justin Tadlock
  * @link      http://themehybrid.com/docs/tutorials/loop-pagination
