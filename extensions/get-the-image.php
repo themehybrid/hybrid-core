dcb3feb1010a036d8096a62404bb038e484bc46e
Jutin Tadlock <justin@justintadlock.com>
2012-12-18 20:33:57 -0600

    Version bump to 0.8.1 for the Get the Image extension.

--- extensions/get-the-image.php
+++ extensions/get-the-image.php
@@ -16,7 +16,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package   GetTheImage
- * @version   0.8.1 - Alpha
+ * @version   0.8.1
  * @author    Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2008 - 2012, Justin Tadlock
  * @link      http://justintadlock.com/archives/2008/05/27/get-the-image-wordpress-plugin
