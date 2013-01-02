bd79f3a67e367267144c12639e67ec1aa72a3059
Jutin Tadlock <justin@justintadlock.com>
2013-01-02 12:21:21 -0600

    Post Stylesheets 0.4.0.

--- extensions/post-stylesheets.php
+++ extensions/post-stylesheets.php
@@ -15,7 +15,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package   PostStylesheets
- * @version   0.4.0 - Alpha
+ * @version   0.4.0
  * @author    Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2010 - 2012, Justin Tadlock
  * @link      http://justintadlock.com
