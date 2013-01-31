803ec4953c9b684910b61213ab8ab117bb54e5ed
Justin Tadlock <justin@justintadlock.com>
2013-01-31 13:48:42 -0600

    Version bump to 1.5.2.

--- hybrid.php
+++ hybrid.php
@@ -24,7 +24,7 @@
  * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * @package   HybridCore
- * @version   1.5.1
+ * @version   1.5.2
  * @author    Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2008 - 2013, Justin Tadlock
  * @link      http://themehybrid.com/hybrid-core
