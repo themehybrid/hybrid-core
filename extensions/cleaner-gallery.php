d77cb959bf7f938acc1acc97eef98c80148ee3d9
Justin Tadlock <justin@justintadlock.com>
2013-02-08 15:38:58 -0600

    Bump Cleaner Gallery to 0.9.6-alpha.

--- extensions/cleaner-gallery.php
+++ extensions/cleaner-gallery.php
@@ -15,9 +15,9 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package   CleanerGallery
- * @version   0.9.5
+ * @version   0.9.6-alpha
  * @author    Justin Tadlock <justin@justintadlock.com>
- * @copyright Copyright (c) 2008 - 2012, Justin Tadlock
+ * @copyright Copyright (c) 2008 - 2013, Justin Tadlock
  * @link      http://justintadlock.com/archives/2008/04/13/cleaner-wordpress-gallery-plugin
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
