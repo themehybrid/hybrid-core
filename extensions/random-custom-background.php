5bce3e72819e3784f97cf6272de9b3642e39798e
Justin Tadlock <justin@justintadlock.com>
2013-02-28 00:35:43 -0600

    Bump Random Custom Background to 0.2.0-alpha.

--- extensions/random-custom-background.php
+++ extensions/random-custom-background.php
@@ -15,7 +15,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package   RandomCustomBackground
- * @version   0.1.0
+ * @version   0.2.0-alpha
  * @author    Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2012, Justin Tadlock
  * @link      http://justintadlock.com
