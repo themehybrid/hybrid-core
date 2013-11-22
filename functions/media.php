ff1ef826827858eb62e976dcc3163da743d147fc
Justin Tadlock <justin@justintadlock.com>
2013-11-22 12:56:43 -0600

    Add the theme's 'post-thumbnail' size name if it exists. Leave other image size name up to theme authors.

--- functions/media.php
+++ functions/media.php
@@ -14,27 +14,21 @@
 add_filter( 'image_size_names_choose', 'hybrid_image_size_names_choose' );
 
 /**
- * Adds theme/plugin custom images sizes added with add_image_size() to the image uploader/editor.  This 
- * allows users to insert these images within their post content editor.
+ * Adds theme "post-thumbnail" size plus an internationalized version of the image size name to the 
+ * "add media" modal.  This allows users to insert the image within their post content editor.
  *
- * @since 1.3.0
+ * @since  1.3.0
  * @access private
- * @param array $sizes Selectable image sizes.
- * @return array $sizes
+ * @param  array   $sizes  Selectable image sizes.
+ * @return array
  */
 function hybrid_image_size_names_choose( $sizes ) {
+	global $_wp_additional_image_sizes;
 
-	/* Get all intermediate image sizes. */
-	$intermediate_sizes = get_intermediate_image_sizes();
-	$add_sizes = array();
+	/* If the theme as set a custom post thumbnail size, give it a nice name. */
+	if ( isset( $_wp_additional_image_sizes['post-thumbnail'] ) )
+		$sizes['post-thumbnail'] = __( 'Post Thumbnail', 'hybrid-core' );
 
-	/* Loop through each of the intermediate sizes, adding them to the $add_sizes array. */
-	foreach ( $intermediate_sizes as $size )
-		$add_sizes[ $size ] = 'post-thumbnail' === $size ? __( 'Post Thumbnail', 'hybrid-core' ) : $size;
-
-	/* Merge the original array, keeping it intact, with the new array of image sizes. */
-	$sizes = array_merge( $add_sizes, $sizes );
-
-	/* Return the new sizes plus the old sizes back. */
+	/* Return the image size names. */
 	return $sizes;
 }
