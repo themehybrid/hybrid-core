f81ff7a7500364f43b0ef1206f2a1570cdcac462
Justin Tadlock <justin@justintadlock.com>
2014-04-15 22:05:05 -0500

    Correct ID and classes for widgets when registering sidebars.

--- functions/sidebars.php
+++ functions/sidebars.php
@@ -46,7 +46,7 @@ function hybrid_register_sidebars() {
 
 			/* Set up some default sidebar arguments. */
 			$defaults = array(
-				'before_widget' => '<section id="%1$s" class="widget %2$s widget-%2$s">',
+				'before_widget' => '<section id="%1$s" class="widget %2$s">',
 				'after_widget'  => '</section>',
 				'before_title'  => '<h3 class="widget-title">',
 				'after_title'   => '</h3>'
