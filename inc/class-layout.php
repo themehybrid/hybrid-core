a5ec58032730c6ddb9dcf049e22d4d628cceaf3b
Pea Lutz <pea@glocal.coop>
2016-02-04 17:04:39 -0500

    Changed instructions in `inc/class-layout.php` to reference the correct file to use for layout API `inc/functions-layouts.php`

--- inc/class-layout.php
+++ inc/class-layout.php
@@ -2,7 +2,7 @@
 /**
  * Layout class.  This class is for creating new layout objects.  Layout registration is handled via
  * the `Hybrid_Layout_Factory` class in `inc/class-layout-factory.php`.  Theme authors should utilize
- * the API functions in `inc/layouts.php`.
+ * the API functions in `inc/functions-layouts.php`.
  *
  * @package    HybridCore
  * @subpackage Includes
