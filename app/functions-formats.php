44bd7b3d73d30ddaf5c3f1459dd5783cb2fb8fda
Justin Tadlock <justintadlock@gmail.com>
2018-03-20 07:23:16 -0500

    Cleans up `functions-formats.php`. Removes the image content functionality since it's not used.

--- app/functions-formats.php
+++ app/functions-formats.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Functions and filters for handling the output of post formats.  This file is only loaded if
- * themes declare support for `post-formats`.
+ * Post format functions.
+ *
+ * Functions and filters for handling the output of post formats.  This file is
+ * only loaded if themes declare support for `post-formats`.
  *
  * @package    HybridCore
  * @subpackage Includes
@@ -27,16 +29,22 @@ add_action( 'wp_loaded', __NAMESPACE__ . '\structured_post_formats', 0 );
 function structured_post_formats() {
 
 	// Add infinity symbol to aside posts.
-	if ( current_theme_supports( 'post-formats', 'aside' ) )
+	if ( current_theme_supports( 'post-formats', 'aside' ) ) {
+
 		add_filter( 'the_content', __NAMESPACE__ . '\aside_infinity', 9 ); // run before wpautop
+	}
 
 	// Adds the link to the content if it's not in the post.
-	if ( current_theme_supports( 'post-formats', 'link' ) )
+	if ( current_theme_supports( 'post-formats', 'link' ) ) {
+
 		add_filter( 'the_content', __NAMESPACE__ . '\link_content', 9 ); // run before wpautop
+	}
 
 	// Wraps `<blockquote>` around quote posts.
-	if ( current_theme_supports( 'post-formats', 'quote' ) )
+	if ( current_theme_supports( 'post-formats', 'quote' ) ) {
+
 		add_filter( 'the_content', __NAMESPACE__ . '\quote_content' );
+	}
 }
 
 /**
@@ -55,7 +63,8 @@ function clean_post_format_slug( $slug ) {
 /* === Asides === */
 
 /**
- * Adds an infinity character "&#8734;" to the end of the post content on 'aside' posts.
+ * Adds an infinity character "&#8734;" to the end of the post content on
+ * 'aside' posts.
  *
  * @since  1.6.0
  * @access public
@@ -64,33 +73,12 @@ function clean_post_format_slug( $slug ) {
  */
 function aside_infinity( $content ) {
 
-	if ( has_post_format( 'aside' ) && ! is_singular() && ! post_password_required() )
-		$content .= apply_filters( 'hybrid_aside_infinity', sprintf( ' <a class="permalink" href="%s">&#8734;</a>', esc_url( get_permalink() ) ) );
+	if ( has_post_format( 'aside' ) && ! is_singular() && ! post_password_required() ) {
 
-	return $content;
-}
-
-/* === Images === */
-
-/**
- * Adds the post format image to the content if no image is found in the post content.  Note, this is not run
- * by default.  To use, add the filter to 'the_content'.
- *
- * @since  1.6.0
- * @access public
- * @param  string  $content
- * @return string
- */
-function image_content( $content ) {
-
-	if ( has_post_format( 'image' ) && ! post_password_required() ) {
-		preg_match( '/<img.*?>/', $content, $matches );
-
-		if ( empty( $matches ) && function_exists( 'get_the_image' ) )
-			$content = get_the_image( array( 'meta_key' => false, 'size' => 'large', 'link' => false, 'echo' => false ) ) . $content;
-
-		elseif ( empty( $matches ) )
-			$content = get_the_post_thumbnail( get_the_ID(), 'large' ) . $content;
+		$content .= apply_filters(
+			app()->namespace . '/aside_infinity',
+			sprintf( ' <a class="permalink" href="%s">&#8734;</a>', esc_url( get_permalink() ) )
+		);
 	}
 
 	return $content;
@@ -99,8 +87,9 @@ function image_content( $content ) {
 /* === Links === */
 
 /**
- * Filters the content of the link format posts.  Wraps the content in the `make_clickable()` function
- * so that users can enter just a URL into the post content editor.
+ * Filters the content of the link format posts.  Wraps the content in the
+ * `make_clickable()` function so that users can enter just a URL into the post
+ * content editor.
  *
  * @since  1.6.0
  * @access public
@@ -109,8 +98,10 @@ function image_content( $content ) {
  */
 function link_content( $content ) {
 
-	if ( has_post_format( 'link' ) && ! post_password_required() && ! preg_match( '/<a\s[^>]*?href=[\'"](.+?)[\'"]/is', $content ) )
+	if ( has_post_format( 'link' ) && ! post_password_required() && ! preg_match( '/<a\s[^>]*?href=[\'"](.+?)[\'"]/is', $content ) ) {
+
 		$content = make_clickable( $content );
+	}
 
 	return $content;
 }
@@ -118,8 +109,8 @@ function link_content( $content ) {
 /* === Quotes === */
 
 /**
- * Checks if the quote post has a <blockquote> tag within the content.  If not, wraps the entire post
- * content with one.
+ * Checks if the quote post has a `<blockquote>` tag within the content.  If not,
+ * wraps the entire post content with one.
  *
  * @since  1.6.0
  * @access public
@@ -131,8 +122,9 @@ function quote_content( $content ) {
 	if ( has_post_format( 'quote' ) && ! post_password_required() ) {
 		preg_match( '/<blockquote.*?>/', $content, $matches );
 
-		if ( empty( $matches ) )
+		if ( empty( $matches ) ) {
 			$content = "<blockquote>{$content}</blockquote>";
+		}
 	}
 
 	return $content;
