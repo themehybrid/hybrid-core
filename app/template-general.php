1bc510610e82c8f044326f481790a777b3f987ef
Justin Tadlock <justintadlock@gmail.com>
2018-03-22 15:06:27 -0500

    Clean up `template-general.php`.

--- app/template-general.php
+++ app/template-general.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * General template functions.  These functions are for use throughout the theme's various template files.
- * Their main purpose is to handle many of the template tags that are currently lacking in core WordPress.
+ * General template tags.
  *
- * @package    HybridCore
- * @subpackage Includes
- * @author     Justin Tadlock <justintadlock@gmail.com>
- * @copyright  Copyright (c) 2008 - 2017, Justin Tadlock
- * @link       https://themehybrid.com/hybrid-core
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * General template functions.  These functions are for use throughout the
+ * theme's various template files. Their main purpose is to handle many of the
+ * template tags that are currently lacking in core WordPress.
+ *
+ * @package   HybridCore
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2008 - 2018, Justin Tadlock
+ * @link      https://themehybrid.com/hybrid-core
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 namespace Hybrid;
@@ -16,7 +18,7 @@ namespace Hybrid;
 /**
  * Outputs the link back to the site.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -28,7 +30,7 @@ function site_link() {
 /**
  * Returns a link back to the site.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -40,7 +42,7 @@ function get_site_link() {
 /**
  * Displays a link to WordPress.org.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -52,7 +54,7 @@ function wp_link() {
 /**
  * Returns a link to WordPress.org.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -64,7 +66,7 @@ function get_wp_link() {
 /**
  * Displays a link to the parent theme URI.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -76,22 +78,34 @@ function theme_link() {
 /**
  * Returns a link to the parent theme URI.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
 function get_theme_link() {
-	$theme   = wp_get_theme( get_template() );
-	$allowed = array( 'abbr' => array( 'title' => true ), 'acronym' => array( 'title' => true ), 'code' => true, 'em' => true, 'strong' => true );
+
+	$theme = wp_get_theme( \get_template() );
+
+	$allowed = [
+		'abbr'    => [ 'title' => true ],
+		'acronym' => [ 'title' => true ],
+		'code'    => true,
+		'em'      => true,
+		'strong'  => true
+	];
 
 	// Note: URI is escaped via `WP_Theme::markup_header()`.
-	return sprintf( '<a class="theme-link" href="%s">%s</a>', $theme->display( 'ThemeURI' ), wp_kses( $theme->display( 'Name' ), $allowed ) );
+	return sprintf(
+		'<a class="theme-link" href="%s">%s</a>',
+		$theme->display( 'ThemeURI' ),
+		wp_kses( $theme->display( 'Name' ), $allowed )
+	);
 }
 
 /**
  * Displays a link to the child theme URI.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -103,102 +117,64 @@ function child_theme_link() {
 /**
  * Returns a link to the child theme URI.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
 function get_child_theme_link() {
 
-	if ( ! is_child_theme() )
+	if ( ! is_child_theme() ) {
 		return '';
+	}
 
-	$theme   = wp_get_theme();
-	$allowed = array( 'abbr' => array( 'title' => true ), 'acronym' => array( 'title' => true ), 'code' => true, 'em' => true, 'strong' => true );
+	$theme = wp_get_theme();
+
+	$allowed = [
+		'abbr'    => [ 'title' => true ],
+		'acronym' => [ 'title' => true ],
+		'code'    => true,
+		'em'      => true,
+		'strong'  => true
+	];
 
 	// Note: URI is escaped via `WP_Theme::markup_header()`.
-	return sprintf( '<a class="child-link" href="%s">%s</a>', $theme->display( 'ThemeURI' ), wp_kses( $theme->display( 'Name' ), $allowed ) );
+	return sprintf(
+		'<a class="child-link" href="%s">%s</a>',
+		$theme->display( 'ThemeURI' ),
+		wp_kses( $theme->display( 'Name' ), $allowed )
+	);
 }
 
 /**
- * Gets the "blog" (posts page) page URL.  `home_url()` will not always work for this because it
- * returns the front page URL.  Sometimes the blog page URL is set to a different page.  This
- * function handles both scenarios.
+ * Gets the "blog" (posts page) page URL.  `home_url()` will not always work for
+ * this because it returns the front page URL.  Sometimes the blog page URL is
+ * set to a different page.  This function handles both scenarios.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
 function get_blog_url() {
 
-	if ( 'posts' === get_option( 'show_on_front' ) )
+	$blog_url = '';
+
+	if ( 'posts' === get_option( 'show_on_front' ) ) {
 		$blog_url = home_url();
 
-	elseif ( 0 < ( $page_for_posts = get_option( 'page_for_posts' ) ) )
+	} elseif ( 0 < ( $page_for_posts = get_option( 'page_for_posts' ) ) ) {
 		$blog_url = get_permalink( $page_for_posts );
+	}
 
-	return ! empty( $blog_url ) ? esc_url( $blog_url ) : '';
+	return $blog_url ? esc_url( $blog_url ) : '';
 }
 
 /**
- * Outputs the site title.
+ * Function for figuring out if we're viewing a "plural" page.  In WP, these
+ * pages are archives, search results, and the home/blog posts index.  Note that
+ * this is similar to, but not quite the same as `!is_singular()`, which wouldn't
+ * account for the 404 page.
  *
- * @since  0.1.0
- * @access public
- * @return void
- */
-function site_title() {
-
-	echo get_site_title();
-}
-
-/**
- * Returns the linked site title wrapped in an `<h1>` tag.
- *
- * @since  2.0.0
- * @access public
- * @return string
- */
-function get_site_title() {
-
-	if ( $title = get_bloginfo( 'name' ) )
-		$title = sprintf( '<h1 %s><a href="%s" rel="home">%s</a></h1>', get_attr( 'site-title' ), esc_url( home_url() ), $title );
-
-	return apply_filters( 'hybrid_site_title', $title );
-}
-
-/**
- * Outputs the site description.
- *
- * @since  0.1.0
- * @access public
- * @return void
- */
-function site_description() {
-
-	echo get_site_description();
-}
-
-/**
- * Returns the site description wrapped in an `<h2>` tag.
- *
- * @since  2.0.0
- * @access public
- * @return string
- */
-function get_site_description() {
-
-	if ( $desc = get_bloginfo( 'description' ) )
-		$desc = sprintf( '<h2 %s>%s</h2>', get_attr( 'site-description' ), $desc );
-
-	return apply_filters( 'hybrid_site_description', $desc );
-}
-
-/**
- * Function for figuring out if we're viewing a "plural" page.  In WP, these pages are archives,
- * search results, and the home/blog posts index.  Note that this is similar to, but not quite
- * the same as `!is_singular()`, which wouldn't account for the 404 page.
- *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return bool
  */
@@ -210,7 +186,7 @@ function is_plural() {
 /**
  * Print the general archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -222,7 +198,7 @@ function single_archive_title() {
 /**
  * Retrieve the general archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -234,7 +210,7 @@ function get_single_archive_title() {
 /**
  * Print the author archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -246,7 +222,7 @@ function single_author_title() {
 /**
  * Retrieve the author archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -258,7 +234,7 @@ function get_single_author_title() {
 /**
  * Print the year archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -270,7 +246,7 @@ function single_year_title() {
 /**
  * Retrieve the year archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -282,7 +258,7 @@ function get_single_year_title() {
 /**
  * Print the week archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -294,20 +270,24 @@ function single_week_title() {
 /**
  * Retrieve the week archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
 function get_single_week_title() {
 
-	// Translators: 1 is the week number and 2 is the year.
-	return sprintf( esc_html__( 'Week %1$s of %2$s', 'hybrid-core' ), get_the_time( esc_html_x( 'W', 'weekly archives date format', 'hybrid-core' ) ), get_the_time( esc_html_x( 'Y', 'yearly archives date format', 'hybrid-core' ) ) );
+	return sprintf(
+		// Translators: 1 is the week number and 2 is the year.
+		esc_html__( 'Week %1$s of %2$s', 'hybrid-core' ),
+		get_the_time( esc_html_x( 'W', 'weekly archives date format', 'hybrid-core' ) ),
+		get_the_time( esc_html_x( 'Y', 'yearly archives date format', 'hybrid-core' ) )
+	);
 }
 
 /**
  * Print the day archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -319,7 +299,7 @@ function single_day_title() {
 /**
  * Retrieve the day archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -331,7 +311,7 @@ function get_single_day_title() {
 /**
  * Print the hour archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -343,7 +323,7 @@ function single_hour_title() {
 /**
  * Retrieve the hour archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -355,7 +335,7 @@ function get_single_hour_title() {
 /**
  * Print the minute archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -367,20 +347,23 @@ function single_minute_title() {
 /**
  * Retrieve the minute archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
 function get_single_minute_title() {
 
-	// Translators: Minute archive title. %s is the minute time format.
-	return sprintf( esc_html__( 'Minute %s', 'hybrid-core' ), get_the_time( esc_html_x( 'i', 'minute archives time format', 'hybrid-core' ) ) );
+	return sprintf(
+		// Translators: Minute archive title. %s is the minute time format.
+		esc_html__( 'Minute %s', 'hybrid-core' ),
+		get_the_time( esc_html_x( 'i', 'minute archives time format', 'hybrid-core' ) )
+	);
 }
 
 /**
  * Print the minute + hour archive title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -392,7 +375,7 @@ function single_minute_hour_title() {
 /**
  * Retrieve the minute + hour archive title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
@@ -404,7 +387,7 @@ function get_single_minute_hour_title() {
 /**
  * Print the search results title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -416,20 +399,23 @@ function search_title() {
 /**
  * Retrieve the search results title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
 function get_search_title() {
 
-	// Translators: %s is the search query.
-	return sprintf( esc_html__( 'Search results for: %s', 'hybrid-core' ), get_search_query() );
+	return sprintf(
+		// Translators: %s is the search query.
+		esc_html__( 'Search results for: %s', 'hybrid-core' ),
+		get_search_query()
+	);
 }
 
 /**
  * Retrieve the 404 page title.
  *
- * @since  2.0.0
+ * @since  5.0.0
  * @access public
  * @return void
  */
@@ -441,7 +427,7 @@ function error_title() {
 /**
  * Retrieve the 404 page title.
  *
- * @since  3.0.0
+ * @since  5.0.0
  * @access public
  * @return string
  */
