fbbcedbfa5a207dd35881cd8c2740476a7d12bf0
Justin Tadlock <justintadlock@gmail.com>
2018-03-25 10:15:02 -0500

    Adds a comment hierarchy function for use with things like finding a comment template.

--- app/template-comments.php
+++ app/template-comments.php
@@ -17,6 +17,28 @@ namespace Hybrid;
 add_filter( 'comments_template', __NAMESPACE__ . '\comments_template', 5 );
 
 /**
+ * Returns a hierarchy for the current comment.
+ *
+ * @since  5.0.0
+ * @access public
+ * @return array
+ */
+function get_comment_hierarchy() {
+
+	$hier = [];
+	$type = get_comment_type() ?: 'comment';
+
+	$hier[] = $type;
+
+	if ( in_array( $type, [ 'pingback', 'trackback'] ) ) {
+
+		$hier[] = 'ping';
+	}
+
+	return apply_filters( 'hybrid/comment_hierarchy', $hier );
+}
+
+/**
  * Outputs the comment reply link.  Only use outside of `wp_list_comments()`.
  *
  * @since  5.0.0
@@ -108,81 +130,6 @@ function get_comment_parent_link( $args = [] ) {
 	}
 
 	return apply_filters( app()->namespace . '/comment_parent_link', $link, $args );
-}
-
-/**
- * Uses the `$comment_type` to determine which comment template should be used.
- * Once the template is located, it is loaded for use. Child themes can create
- * custom templates based off the `$comment_type`. The comment template hierarchy
- * is `comment-$comment_type.php`, `comment.php`.
- *
- * The templates are saved in `\Hybrid\app()->comment_templates[ $comment_type ]`,
- * so each comment template is only located once if it is needed. Following
- * comments will use the saved template.
- *
- * @since  5.0.0
- * @access public
- * @param  object  $comment
- * @return void
- */
-function comments_callback( $comment ) {
-
-	// Get the comment type of the current comment.
-	$comment_type = get_comment_type( $comment->comment_ID );
-
-	// Check if a template has been provided for the specific comment type.
-	// If not, get the template.
-	if ( ! isset( app()->comment_templates[ $comment_type ] ) ) {
-
-		// Create an array of template files to look for.
-		$templates = [
-			"comment-{$comment_type}.php",
-			"comment/{$comment_type}.php"
-		];
-
-		// If the comment type is a 'pingback' or 'trackback', allow the use of 'comment-ping.php'.
-		if ( 'pingback' == $comment_type || 'trackback' == $comment_type ) {
-			$templates[] = 'comment-ping.php';
-			$templates[] = 'comment/ping.php';
-		}
-
-		// Add the fallback 'comment.php' template.
-		$templates[] = 'comment/comment.php';
-		$templates[] = 'comment.php';
-
-		// Allow devs to filter the template hierarchy.
-		$templates = apply_filters(
-			app()->namespace . '/comment_template_hierarchy',
-			$templates,
-			$comment_type
-		);
-
-		// Locate the comment template.
-		$template = locate_template( $templates );
-
-		// Set the template in the comment templates array.
-		app()->comment_templates[ $comment_type ] = $template;
-	}
-
-	// If a template was found, load the template.
-	if ( ! empty( app()->comment_templates[ $comment_type ] ) ) {
-		require( app()->comment_templates[ $comment_type ] );
-	}
-}
-
-/**
- * Ends the display of individual comments. Uses the callback parameter for
- * `wp_list_comments()`. Needs to be used in conjunction with
- * `hybrid_comments_callback()`. Not needed but used just in case something is
- * changed.
- *
- * @since  5.0.0
- * @access public
- * @return void
- */
-function comments_end_callback() {
-
-	echo '</li><!-- .comment -->';
 }
 
 /**
