9ae9848ec45ce8ace88b550f022316ba8c8d8a80
Justin Tadlock <justintadlock@gmail.com>
2018-03-28 13:02:08 -0500

    Look for view templates in `public/views`.

--- config/view.php
+++ config/view.php
@@ -10,7 +10,7 @@ return [
 	 | The path, relative to the theme root, where view templates exist.
 	 |
 	 */
-	 'path' => 'resources/views',
+	 'path' => 'public/views',
 
 	/*
 	 |-------------------------------------------------------------------------
