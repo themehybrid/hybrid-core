663484d622299ca9a786d4d0c6179ddbc415160e
Justin Tadlock <justintadlock@gmail.com>
2018-04-28 16:47:15 -0500

    Add some missing inline docs.

--- src/attributes/class-attributes-service-provider.php
+++ src/attributes/class-attributes-service-provider.php
@@ -1,16 +1,45 @@
 <?php
+/**
+ * Attributes service provider.
+ *
+ * This is the service provider for the attributes system. It's used to bind the
+ * `Attributes` class to the container.
+ *
+ * @package   HybridCore
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2008 - 2018, Justin Tadlock
+ * @link      https://themehybrid.com/hybrid-core
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
 
 namespace Hybrid\Attributes;
 
 use Hybrid\Core\ServiceProvider;
 
+/**
+ * Attributes provider class.
+ *
+ * @since  5.0.0
+ * @access public
+ */
 class AttributesServiceProvider extends ServiceProvider {
 
+        /**
+         * Registration callback that binds the `Attributes` class to the container.
+         *
+         * @since  5.0.0
+         * @access public
+         * @return void
+         */
         public function register() {
 
                 $this->app->add( 'attr', function( $container, $params ) {
 
-                        return new Attributes( $params['name'], $params['context'], $params['attr'] );
+                        return new Attributes(
+                                $params['name'],
+                                $params['context'],
+                                $params['attr']
+                        );
                 } );
         }
 }
--- src/view/class-view-service-provider.php
+++ src/view/class-view-service-provider.php
@@ -1,12 +1,37 @@
 <?php
+/**
+ * View service provider.
+ *
+ * This is the service provider for the view system. It's used to bind the
+ * `View` class to the container.
+ *
+ * @package   HybridCore
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2008 - 2018, Justin Tadlock
+ * @link      https://themehybrid.com/hybrid-core
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
 
 namespace Hybrid\View;
 
 use Hybrid\Core\Collection;
 use Hybrid\Core\ServiceProvider;
 
+/**
+ * View provider class.
+ *
+ * @since  5.0.0
+ * @access public
+ */
 class ViewServiceProvider extends ServiceProvider {
 
+        /**
+         * Registration callback that binds the `View` class to the container.
+         *
+         * @since  5.0.0
+         * @access public
+         * @return void
+         */
         public function register() {
 
                 $this->app->add( 'view', function( $container, $params ) {
