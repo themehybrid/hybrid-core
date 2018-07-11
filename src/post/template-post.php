726acb4463b21d6881d1c9aaefdda986f606011d
Justin Tadlock <justintadlock@gmail.com>
2018-07-11 09:58:20 -0500

    Drop `TemplateTag` class for now and just stick with plain functions. This was experimental, but I didn't feel it was right just yet.

--- src/post/template-post.php
+++ src/post/template-post.php
@@ -2,52 +2,162 @@
 
 namespace Hybrid\Post;
 
+use function Hybrid\Attr\attr;
+
 function render_author( array $args = [] ) {
 
-	( new Author( $args ) )->render();
+	echo fetch_author( $args );
 }
 
 function fetch_author( array $args = [] ) {
 
-	return ( new Author( $args ) )->fetch();
+	$args = wp_parse_args( $args, [
+		'text'   => '%s',
+		'before' => '',
+		'after'  => ''
+	] );
+
+	$attr = [
+		'href'  => get_author_posts_url( get_the_author_meta( 'ID' ) ),
+		'class' => 'entry__author'
+	];
+
+	$html = sprintf(
+		'<a %s>%s</a>',
+		attr( 'entry-author', '', $attr )->fetch(),
+		sprintf( $args['text'], get_the_author() )
+	);
+
+	return $args['before'] . $html . $args['after'];
 }
 
 function render_date( array $args = [] ) {
 
-	( new Date( $args ) )->render();
+	echo fetch_date( $args );
 }
 
 function fetch_date( array $args = [] ) {
 
-	return ( new Date( $args ) )->fetch();
+	$args = wp_parse_args( $args, [
+		'text'   => '%s',
+		'format' => '',
+		'before' => '',
+		'after'  => ''
+	] );
+
+	$attr = [ 'class' => 'entry__published' ];
+
+	$html = sprintf(
+		'<time %s>%s</time>',
+		attr( 'entry-published', '', $attr ),
+		sprintf( $args['text'], get_the_date( $args['format'] ) )
+	);
+
+	return $args['before'] . $html . $args['after'];
 }
 
 function render_comments_link( array $args = [] ) {
 
-	( new CommentsLink( $args ) )->render();
+	echo fetch_comments_link( $args );
 }
 
 function fetch_comments_link( array $args = [] ) {
 
-	return ( new CommentsLink( $args ) )->fetch();
+	$args = wp_parse_args( $args, [
+		'zero'   => false,
+		'one'    => false,
+		'more'   => false,
+		'before' => '',
+		'after'  => ''
+	] );
+
+	$number = get_comments_number();
+
+	if ( 0 == $number && ! comments_open() && ! pings_open() ) {
+		return '';
+	}
+
+	$attr = [
+		'href'  => get_comments_link(),
+		'class' => 'entry__comments'
+	];
+
+	$text = get_comments_number( $args['zero'], $args['one'], $args['more'] );
+
+	$html = sprintf(
+		'<a %s>%s</a>',
+		attr( 'entry-comments', '', $attr ),
+		$text
+	);
+
+	return $args['before'] . $html . $args['after'];
 }
 
 function render_terms( array $args = [] ) {
 
-	( new Terms( $args ) )->render();
+	echo fetch_terms( $args );
 }
 
 function fetch_terms( array $args = [] ) {
 
-	return ( new Terms( $args ) )->fetch();
+	$html = '';
+
+	$args = wp_parse_args( $args, [
+		'taxonomy' => 'category',
+		'text'     => '%s',
+		// Translators: Separates tags, categories, etc. when displaying a post.
+		'sep'      => _x( ', ', 'taxonomy terms separator', 'hybrid-core' ),
+		'before'   => '',
+		'after'    => ''
+	] );
+
+	$terms = get_the_term_list( get_the_ID(), $args['taxonomy'], '', $args['sep'], '' );
+
+	if ( $terms ) {
+
+		$attr = [
+			'class' => "entry__terms entry__terms--{$args['taxonomy']}"
+		];
+
+		$html = sprintf(
+			'<span %s>%s</span>',
+			attr( 'entry-terms', $args['taxonomy'], $attr )->fetch(),
+			sprintf( $args['text'], $terms )
+		);
+
+		$html = $args['before'] . $html . $args['after'];
+	}
+
+	return $html;
 }
 
 function render_format( array $args = [] ) {
 
-	( new Format( $args ) )->render();
+	echo fetch_format( $args );
 }
 
 function fetch_format( array $args = [] ) {
 
-	return ( new Format( $args ) )->fetch();
+	$args = wp_parse_args( $args, [
+		'text'   => '%s',
+		'before' => '',
+		'after'  => ''
+	] );
+
+	$format = get_post_format();
+	$url    = $format ? get_post_format_link( $format ) : get_permalink();
+	$string = get_post_format_string( $format );
+
+	$attr = [
+		'href'  => $url,
+		'class' => 'entry__format'
+	];
+
+	$html = sprintf(
+		'<a %s>%s</a>',
+		attr( 'entry-format', '', $attr )->fetch(),
+		sprintf( $args['text'], $string )
+	);
+
+	return $args['before'] . $html . $args['after'];
 }
--- src/site/template-site.php
+++ src/site/template-site.php
@@ -2,22 +2,73 @@
 
 namespace Hybrid\Site;
 
+use function Hybrid\Attr\attr;
+
 function render_title( array $args = [] ) {
 
-	( new Title( $args ) )->render();
+	echo fetch_title( $args );
 }
 
 function fetch_title( array $args = [] ) {
 
-	return ( new Title( $args ) )->fetch();
+	$args = wp_parse_args( $args, [
+		'tag'       => is_front_page() ? 'h1' : 'div',
+		'component' => 'app-header'
+	] );
+
+	$html  = '';
+	$title = get_bloginfo( 'name', 'display' );
+
+	if ( $title ) {
+		$link = sprintf( '<a href="%s">%s</a>', esc_url( home_url() ), $title );
+
+		$attr = [];
+
+		if ( $args['component'] ) {
+			$attr['class'] = "{$args['component']}__title";
+		}
+
+		$html = sprintf(
+			'<%1$s %2$s>%3$s</%1$s>',
+			tag_escape( $args['tag'] ),
+			attr( 'site-title', $args['component'], $attr )->fetch(),
+			$link
+		);
+	}
+
+	return apply_filters( 'hybrid/site/title', $html );
 }
 
 function render_description( array $args = [] ) {
 
-	( new Description( $args ) )->render();
+	echo fetch_description( $args );
 }
 
 function fetch_description( array $args = [] ) {
 
-	return ( new Description( $args ) )->fetch();
+	$args = wp_parse_args( $args, [
+		'tag'       => 'div',
+		'component' => 'app-header',
+	] );
+
+	$html = '';
+	$desc = get_bloginfo( 'description', 'display' );
+
+	if ( $desc ) {
+
+		$attr = [];
+
+		if ( $args['component'] ) {
+			$attr['class'] = "{$args['component']}__description";
+		}
+
+		$html = sprintf(
+			'<%1$s %2$s>%3$s</%1$s>',
+			tag_escape( $args['tag'] ),
+			attr( 'site-description', $args['component'], $attr )->fetch(),
+			$desc
+		);
+	}
+
+	return apply_filters( 'hybrid/site/description', $html );
 }
