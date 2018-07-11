1fa7ec039d1fb79959fcfc7c2d5a415d7c01760e
Justin Tadlock <justintadlock@gmail.com>
2018-07-11 16:39:51 -0500

    Remove leftover `get_post_hierarchy()` function.

--- src/template-general.php
+++ src/template-general.php
@@ -29,53 +29,6 @@ function get_global_hierarchy() {
 }
 
 /**
- * Creates a hierarchy based on the current post. It's primary purpose is for
- * use with post views/templates.
- *
- * @since  5.0.0
- * @access public
- * @return array
- */
-function get_post_hierarchy() {
-
-	// Set up an empty array and get the post type.
-	$hierarchy = [];
-	$post_type = get_post_type();
-
-	// If attachment, add attachment type templates.
-	if ( 'attachment' === $post_type ) {
-
-		$type    = get_attachment_type();
-		$subtype = get_attachment_subtype();
-
-		if ( $subtype ) {
-			$hierarchy[] = "attachment-{$type}-{$subtype}";
-			$hierarchy[] = "attachment-{$subtype}";
-		}
-
-		$hierarchy[] = "attachment-{$type}";
-	}
-
-	// If the post type supports 'post-formats', get the template based on the format.
-	if ( post_type_supports( $post_type, 'post-formats' ) ) {
-
-		// Get the post format.
-		$post_format = get_post_format() ?: 'standard';
-
-		// Template based off post type and post format.
-		$hierarchy[] = "{$post_type}-{$post_format}";
-
-		// Template based off the post format.
-		$hierarchy[] = $post_format;
-	}
-
-	// Template based off the post type.
-	$hierarchy[] = $post_type;
-
-	return apply_filters( 'hybrid/hierarchy/post', $hierarchy );
-}
-
-/**
  * Gets the "blog" (posts page) page URL.  `home_url()` will not always work for
  * this because it returns the front page URL.  Sometimes the blog page URL is
  * set to a different page.  This function handles both scenarios.
