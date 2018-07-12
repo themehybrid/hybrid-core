663e898ac57c54456e424cfba367a4ad1d239597
Justin Tadlock <justintadlock@gmail.com>
2018-07-12 16:37:04 -0500

    Follow the same convention of `functions-{$name}.php` for functions file in all sub-folders.

--- src/bootstrap-functions.php
+++ src/bootstrap-functions.php
@@ -14,34 +14,36 @@
  */
 
 # Autoloads our custom functions files that are not loaded via the class loader.
-array_map(
-	function( $file ) {
-		require_once( "{$file}.php" );
-	},
-	[
-		'functions-assets',
-		'functions-attr',
-		'functions-context',
-		'functions-filters',
-		'functions-fonts',
-		'functions-formats',
-		'functions-head',
-		'functions-helpers',
-		'functions-lang',
-		'functions-utility',
+array_map( function( $file ) {
+	require_once( "{$file}.php" );
+}, [
+	'functions-assets',
+	'functions-attr',
+	'functions-context',
+	'functions-filters',
+	'functions-fonts',
+	'functions-formats',
+	'functions-head',
+	'functions-helpers',
+	'functions-lang',
+	'functions-utility',
+] );
 
-		'attr/template-attr',
-		'comment/template-comment',
-		'media/template-media',
-		'media-grabber/template-media-grabber',
-		'media-meta/template-media-meta',
-		'menu/template-menu',
-		'pagination/template-pagination',
-		'post/template-post',
-		'sidebar/template-sidebar',
-		'site/template-site',
-		'template/functions-template',
-		'theme/template-theme',
-		'view/template-view'
-	]
-);
+# Autoloads functions files in component folders.
+array_map( function( $name ) {
+	require_once( "{$name}/functions-{$name}.php" );
+}, [
+	'attr',
+	'comment',
+	'media',
+	'media-grabber',
+	'media-meta',
+	'menu',
+	'pagination',
+	'post',
+	'sidebar',
+	'site',
+	'template',
+	'theme',
+	'view'
+] );
