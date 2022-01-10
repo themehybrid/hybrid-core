090c3528df3e48c0360f4577bc28d4eaa856ce2f
saasfreelancer <syed.sayal.shah@gmail.com>
2022-01-10 21:34:28 +0500

    feat: upgrade container
    - add bound, bindIf, make, singletonIf, scoped, scopedIf, tag, tagged, addContextualBinding, when, factory, when, getClosure, rebinding, refresh, rebound, getReboundCallbacks, wrap, call, makeWith, resolved, isShared,  isAlias, removeAbstractAlias, getContextualConcrete, findInContextualBindings, isBuildable, hasParameterOverride, getParameterOverride, getLastParameterOverride, resolvePrimitive, resolveClass, resolveVariadicClass, notInstantiable, unresolvablePrimitive, beforeResolving, resolving, afterResolving, fireBeforeResolvingCallbacks, fireBeforeCallbackArray, fireResolvingCallbacks, fireAfterResolvingCallbacks, getCallbacksForType, fireCallbackArray, getBindings, getAlias, getExtenders, forgetExtenders, dropStaleInstances, forgetInstance, forgetInstances, forgetScopedInstances, flush, getInstance, setInstance functions to container
    
    - Improved PHP 8 compatiblity
    - Sync the HC container with Laravel container v9.9.0
    
    Note: Keep psr/container in sync with WooCommerce psr/container version.

---
+++ src/Util.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Hybrid;
+
+use Closure;
+use ReflectionNamedType;
+
+/**
+ * @internal
+ */
+class Util {
+
+	/**
+	 * If the given value is not an array and not null, wrap it in one.
+	 *
+	 * @param  mixed  $value
+	 * @return array
+	 */
+	public static function arrayWrap( $value ) {
+		if ( is_null( $value ) ) {
+			return [];
+		}
+
+		return is_array( $value ) ? $value : [$value];
+	}
+
+	/**
+	 * Return the default value of the given value.
+	 *
+	 * @param  mixed  $value
+	 * @param  mixed  ...$args
+	 * @return mixed
+	 */
+	public static function unwrapIfClosure( $value, ...$args ) {
+		return $value instanceof Closure ? $value( ...$args ) : $value;
+	}
+
+	/**
+	 * Get the class name of the given parameter's type, if possible.
+	 *
+	 * @param  \ReflectionParameter  $parameter
+	 * @return string|null
+	 */
+	public static function getParameterClassName( $parameter ) {
+		$type = $parameter->getType();
+
+		if ( ! $type instanceof ReflectionNamedType || $type->isBuiltin() ) {
+			return null;
+		}
+
+		$name = $type->getName();
+
+		if ( ! is_null( $class = $parameter->getDeclaringClass() ) ) {
+			if ( $name === 'self' ) {
+				return $class->getName();
+			}
+
+			if ( $name === 'parent' && $parent = $class->getParentClass() ) {
+				return $parent->getName();
+			}
+		}
+
+		return $name;
+	}
+}
